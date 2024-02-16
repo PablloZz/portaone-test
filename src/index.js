014753bc02823d05a7c7e688fd2a456fd34ba7b5
Pavlo Zalutskiy <kyrogane1@gmail.com>
2024-02-16 15:50:10 +0200

    feat: Find min number

--- src/index.js
+++ src/index.js
@@ -6,4 +6,8 @@ const stringList = await readFile(path.join("db", "10m.txt"), "utf8");
 const strings = stringList.split(/\r\n|\n/);
 const numbers = strings.map(string => Number(string));
 numbers.pop();
+const sortedNumbers = numbers.toSorted((first, second) => first - second);
+
+const minNumber = sortedNumbers[0];
+console.log("Min number", minNumber);
 console.timeEnd();
